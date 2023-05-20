843c7619885dc73b04583b1fae867de77b541b13
dongsuk <scienceyds@naver.com>
2023-05-21 00:06:17 +0900

    [refactor] 코드 정리 및 기본 세팅

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import { connectDB } from "/util/database.js";
 
 export default async function Home() {
-  let db = (await connectDB).db("forum");
-  let result = await db.collection("post").find().toArray();
-  return <main>{result[0].title}</main>;
+  // let db = (await connectDB).db("forum");
+  // let result = await db.collection("post").find().toArray();
+  return <main>this is main page</main>;
 }
