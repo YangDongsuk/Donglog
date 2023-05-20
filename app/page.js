bb05e91cdbe0619c7c47112124e05a84a5ac31c9
dongsuk <scienceyds@naver.com>
2023-05-21 02:22:05 +0900

    [feat]메인 페이지 헤드 카드

--- app/page.js
+++ app/page.js
@@ -1,48 +1,65 @@
 "use client";
 import {
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  AccordionIcon,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Stack,
+  Heading,
+  Button,
+  Image,
+  Text,
+  Flex,
   Box,
 } from "@chakra-ui/react";
+
 export default function Home() {
   return (
-    <Accordion>
-      <AccordionItem>
-        <h2>
-          <AccordionButton>
-            <Box as="span" flex="1" textAlign="left">
-              Section 1 title
-            </Box>
-            <AccordionIcon />
-          </AccordionButton>
-        </h2>
-        <AccordionPanel pb={4}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </AccordionPanel>
-      </AccordionItem>
+    <>
+      {/* 메인 페이지 헤드 카드 */}
+      <Card
+        direction={{ base: "column", sm: "row" }}
+        overflow="hidden"
+        variant="outline"
+      >
+        <Image
+          objectFit="cover"
+          maxW={{ base: "100%", sm: "200px" }}
+          src="/rice.jpg"
+          alt="rice"
+        />
 
-      <AccordionItem>
-        <h2>
-          <AccordionButton>
-            <Box as="span" flex="1" textAlign="left">
-              Section 2 title
-            </Box>
-            <AccordionIcon />
-          </AccordionButton>
-        </h2>
-        <AccordionPanel pb={4}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </AccordionPanel>
-      </AccordionItem>
-    </Accordion>
+        <Stack>
+          <CardBody>
+            <Heading size="xl">고봉밥 블로그</Heading>
+
+            <Text py="2">백엔드 개발자지만 남 몰래 프론트 공부중인 개발자</Text>
+          </CardBody>
+
+          <CardFooter className="sns-icons">
+            <Flex direction="row" align="center">
+              <Box mr={2} _hover={{ opacity: "0.7" }}>
+                <a
+                  href="https://github.com/YangDongsuk"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src="/icons/github.svg" alt="GitHub" />
+                </a>
+              </Box>
+              <Box _hover={{ opacity: "0.7" }}>
+                <a
+                  href="https://www.instagram.com/import_dsy/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src="/icons/instagram.svg" alt="Instagram" />
+                </a>
+              </Box>
+            </Flex>
+          </CardFooter>
+        </Stack>
+      </Card>
+    </>
   );
 }
