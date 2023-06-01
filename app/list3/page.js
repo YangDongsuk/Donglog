9597a850f624580463689e42d9763c61adaa2a0c
dongsuk <scienceyds@naver.com>
2023-06-01 21:46:19 +0900

    [fix] 노션 데이어이스 실시간 동기화 안되는 문제 해결

--- app/list3/page.js
+++ app/list3/page.js
@@ -1,17 +1,38 @@
-import ListItem from "./ListItem";
+"use client";
+import { useState, useEffect } from "react";
+import ListTab from "./ListTab";
+//서버 렌더링으로 하면 노션 데이터베이스에서 데이터 동기화가 잘 되지 않기 때문에 클라이언트 렌더링으로 변경
 
-export default async function List() {
-  const res = await fetch("http://localhost:3000/api/notionDatabase/getData", {
-    method: "GET",
-  });
-  const projects = await res.json();
+export default function List() {
+  const [projects, setProjects] = useState(null);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const res = await fetch(
+        "http://localhost:3000/api/notionDatabase/getData",
+        {
+          method: "GET",
+        }
+      );
+      const data = await res.json();
+      setProjects(data);
+    };
+
+    fetchProjects();
+  }, []);
+
+  if (!projects) {
+    return <div>Loading...</div>;
+  }
+
   const filteredResults = projects.results.filter(
     (project) => project.properties.Status.status.name === "Done"
   );
 
   return (
     <div className="list-bg">
-      <ListItem result={filteredResults} />
+      {/* <ListItem result={filteredResults} /> */}
+      <ListTab result={filteredResults} />
     </div>
   );
 }
