eb8f1f64c2879ceda209e31dabfd7ed178fb8d18
dongsuk <scienceyds@naver.com>
2023-06-05 21:34:23 +0900

    [feat] 게시판 최신순 오래된순 정렬 기능 추가

--- app/list3/ListItem.js
+++ app/list3/ListItem.js
@@ -1,6 +1,8 @@
 "use client";
 import Link from "next/link";
+import { useState } from "react";
 import {
+  Button,
   Card,
   CardBody,
   Heading,
@@ -9,20 +11,53 @@ import {
   Box,
   Text,
   useMediaQuery,
+  ButtonGroup,
+  Spacer,
 } from "@chakra-ui/react";
 
 export default function ListItem({ result }) {
   const [isLargerThan800] = useMediaQuery("(min-width: 800px)");
+  const [isSortedNewestFirst, setSortedNewestFirst] = useState(true);
+
+  const sortedResult = [...result].sort((a, b) => {
+    if (isSortedNewestFirst) {
+      return (
+        new Date(b.properties.Date.date.start) -
+        new Date(a.properties.Date.date.start)
+      );
+    } else {
+      return (
+        new Date(a.properties.Date.date.start) -
+        new Date(b.properties.Date.date.start)
+      );
+    }
+  });
+
+  const toggleSorting = () => {
+    setSortedNewestFirst(!isSortedNewestFirst);
+  };
 
   return (
     <div>
+      <ButtonGroup spacing={4}>
+        <Button
+          onClick={toggleSorting}
+          colorScheme={isSortedNewestFirst ? "teal" : "gray"}
+          width="100px"
+        >
+          {isSortedNewestFirst ? "최신순" : "오래된순"}
+        </Button>
+
+        <Spacer />
+      </ButtonGroup>
+
       <Card w={isLargerThan800 ? "768px" : "97%"}>
         <CardBody>
           <Stack divider={<StackDivider />} spacing="4">
-            {result.map((item, i) => {
+            {sortedResult.map((item, i) => {
               return (
                 <Box key={i}>
-                  <Link href={"posting2/" + result[i].id}>
+                  <Link href={"posting2/" + item.id}>
                     <Heading size="xxl">
                       {item.properties.Name.title[0].text.content}
                     </Heading>
